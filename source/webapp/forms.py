acf0ff7b0591af9d728ce1f2b80db06c8d5b6f50
Evgenii <neroznik@yandex.ru>
2020-08-29 12:27:12 +0600

    Созданы формы forms.py

---
+++ source/webapp/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+from webapp.models import Poll, Choice
+
+
+class ProjectsForm(forms.ModelForm):
+    class Meta:
+        model = Poll
+        fields = ['question']
+
+
+class ChoiceForm(forms.ModelForm):
+    class Meta:
+        model = Choice
+        fields = ['option']
