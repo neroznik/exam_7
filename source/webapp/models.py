bb857991c62a927bed9ba0897969bd1607998ac5
Evgenii <neroznik@yandex.ru>
2020-08-29 17:18:39 +0600

    Добавлена модель  Answer

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -3,8 +3,8 @@ from django.utils import timezone
 from django.db import models
 
 class Poll(models.Model):
-    question = models.TextField(max_length=2000, verbose_name='Вопрос')
-    created_at = models.DateTimeField(verbose_name='Время создания', default=timezone.now)
+    question = models.TextField(max_length=2000, verbose_name='Question')
+    created_at = models.DateTimeField(verbose_name='Created at', default=timezone.now)
 
 
     class Meta:
@@ -17,10 +17,16 @@ class Poll(models.Model):
 
 
 class Choice(models.Model):
-    option = models.TextField(max_length=2000, verbose_name='Вариант ответа')
+    option = models.TextField(max_length=2000, verbose_name='Options')
     poll = models.ForeignKey('webapp.Poll', related_name='choices', on_delete=models.CASCADE, verbose_name='Опрос')
 
     def __str__(self):
         return "{}. {}".format(self.pk, self.option)
+
+
+class Answer(models.Model):
+    poll = models.ForeignKey('webapp.Poll', related_name='answer', on_delete=models.CASCADE)
+    time = models.DateTimeField(verbose_name='Time of answer', default=timezone.now)
+    choption = models.ForeignKey('webapp.Choice', related_name='choption', on_delete=models.CASCADE, verbose_name=" Chosen option")
 
 
